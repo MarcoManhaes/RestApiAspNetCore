6de00b7fa0161154f08d4ea0e66379d69b7e4cc4
Marco Júnior <marco.junior@groupsoftware.com.br>
2020-07-09 22:02:17 -0300

    eitaaaa

---
+++ RestApiAspNetCore/RestApiAspNetCore/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace RestApiAspNetCore.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Adress { get; set; }
+        public long Gender { get; set; }
+    }
+}
